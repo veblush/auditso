a8e01f9e7a0281edb9a2973ac284e7bf2c487f91
Esun Kim <veblush@google.com>
2019-08-27 09:07:31 -0700

    Clean up source

--- auditso.sh
+++ auditso.sh
@@ -6,23 +6,36 @@ usage() {
   echo "usage: $0 test.so"
 }
 
-audit() {
-  tmp_dir=$(mktemp -d -t auditso-XXXXXXXXXX)
-  trap "{ rm -rf $tmp_dir; }" EXIT
-
+make_conda_package() {
+  # Create a conda package. The generated file is not fully legit
+  # but enough for auditwheel to understand.
+  # (https://docs.conda.io/projects/conda-build/en/latest/resources/package-spec.html)
   target=$1
+  tmp_dir=$2
+  pack_file=$3
   filename=$(basename $target)
 
   pack_dir=$tmp_dir/pack
-  pack_file=$tmp_dir/pack.tar.bz2 
   mkdir $pack_dir
   cp $target $pack_dir/$filename
   
   mkdir $pack_dir/info
   echo $filename > $pack_dir/info/files
   tar -cjSf $pack_file -C $pack_dir .
+}
 
+audit() {
+  # Create a temporary conda package to test it compliant with manylinux1
+  tmp_dir=$(mktemp -d -t auditso-XXXXXXXXXX)
+  trap "{ rm -rf $tmp_dir; }" EXIT
+
+  pack_file=$tmp_dir/pack.tar.bz2 
+  make_conda_package $target $tmp_dir $pack_file
+
+  # Run auditwheel against the generated package
   auditwheel show $pack_file > $tmp_dir/output
+
+  # If output has "manylinux1, it is considered successful.
   if grep -q "\"manylinux1" $tmp_dir/output
   then
       echo "* Pass: Compliant with manylinux1."
